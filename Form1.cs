b2ef1dfa44c40f1cd8935fc89f8aa977885871ca
biielchagaas <gabriel14chagas@gmail.com>
2023-06-15 09:30:07 -0400

    Cálculo deu certogit add . :P

--- Form1.cs
+++ Form1.cs
@@ -11,15 +11,16 @@ namespace Prova2Bim
         private void button1_Click(object sender, EventArgs e)
         {
             double valor = Convert.ToDouble(tx_valor.Text);
-            int pis = Convert.ToInt32(tx_pis.Text);
-            int icms = Convert.ToInt32(tx_icms.Text);
-            int confins = Convert.ToInt32(tx_confins.Text);
-            int lucro = Convert.ToInt32(tx_lucro.Text);
+            double pis = Convert.ToDouble(tx_pis.Text);
+            double icms = Convert.ToDouble(tx_icms.Text);
+            double confins = Convert.ToDouble(tx_confins.Text);
+            double lucro = Convert.ToDouble(tx_lucro.Text);
             string unidade = cb_unidade.Text;
 
             Produto produto = new Produto(valor, pis, icms, confins, lucro);
             double resultado = produto.Calculo(unidade);
-            lb_vlrVenda.Text = Convert.ToString(resultado);
+            MessageBox.Show(resultado.ToString("c2"));
+            lb_vlrVenda.Text = resultado.ToString("c2");
         }
     }
 }
--- Produto.cs
+++ Produto.cs
@@ -3,14 +3,14 @@
 public class Produto
 {
     public double valor;
-    public int pis;
-    public int icms;
-    public int confins;
-    public int lucro;
-    double calc = 0;
+    public double pis;
+    public double icms;
+    public double confins;
+    public double lucro;
+    double[] calc = new double[5];
     double soma = 0;
 
-    public Produto(double valor, int pis, int icms, int confins, int lucro)
+    public Produto(double valor, double pis, double icms, double confins, double lucro)
     {
         this.valor = valor;
         this.pis = pis;
@@ -21,23 +21,21 @@ public class Produto
 
     public double Calculo(string unidade)
     {
-        calc = (valor * (pis / 100)) + valor;
-        soma += calc;
-        calc = valor * (icms / 100);
-        soma += calc;
-        calc = valor * (confins / 100);
-        soma += calc;
-        calc = soma * (lucro / 100);
-        soma += calc;
+        calc[0] = valor * (pis / 100);
+        calc[1] = valor * (icms / 100);
+        calc[2] = valor * (confins / 100);
+        soma = valor + calc[0] + calc[1] + calc[2];
+        calc[3] = soma * (lucro / 100);
+        soma = soma + calc[3];
         if (unidade == "LT" || unidade == "KG")
         {
-            calc = soma * 0.05;
-            soma += calc;
-            return calc;
+            calc[4] = soma * 0.05;
+            soma = soma + calc[4];
+            return soma;
         }
         else
         {
-            return calc;
+            return soma;
         }
     }
 }
